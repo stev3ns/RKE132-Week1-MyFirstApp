94c0951e52dba7a65cb5644afaec6b9152fc30a5
Steven S <stevensarap321@gmail.com>
2024-09-04 15:49:23 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// This is a comment to my code
+
+// Rakendus küsib kasutajal sisestada nime
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!"); //Output
